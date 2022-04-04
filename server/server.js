c223f09010cd21199bc4fb879d8f76a70ddf50b8
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-04 02:46:40 -0300

    secure connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const path = require('path')
-const grpc = require('@grpc/grpc-js');
+const grpc = require('@grpc/grpc-js')
+const fs = require('fs')
 // const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader')
 const filepath = path.resolve(__dirname, '../proto/bookshop.proto')
 const packageDefinition = protoLoader.loadSync(filepath)
 const BookshopDefinition = grpc.loadPackageDefinition(packageDefinition)
+
+const caCertPath = path.resolve(__dirname, '../credentials/ca.cert')
+const serverCertPath = path.resolve(__dirname, '../credentials/server.cert')
+const serverKeyPath = path.resolve(__dirname, '../credentials/server.key')
 
 const mongodb = require('mongodb')
 const mongoClient = mongodb.MongoClient
@@ -34,12 +39,12 @@ function getAllBooks() {
 	return new Promise(function(resolve, reject) {
 		db.collection('books').find({}).toArray((err, result) => {
 			if (err) {
-				reject(error);
+				reject(err);
 			} 
 
 			resolve(result.map(item => mapBook(item)));
-		})
-	})
+		});
+	});
 }
 
 function findBook(id) {
@@ -50,8 +55,8 @@ function findBook(id) {
 			} 
 
 			resolve(mapBook(result));
-		})
-	})
+		});
+	});
 }
 
 function addBook(book) {
@@ -62,8 +67,8 @@ function addBook(book) {
 			} 
 
 			resolve({insertedId: result.insertedId.toString()});
-		})
-	})
+		});
+	});
 }
 
 function removeBook(id) {
@@ -74,36 +79,63 @@ function removeBook(id) {
 			} 
 
 			resolve();
-		})
-	})
+		});
+	});
 }
 
 function List (_, cb) {
 	getAllBooks()
 	.then(res => {
 		return cb(null, {books: res})
-	})
+	}).catch(err => {
+		return cb({
+			code: grpc.status.UNKNOWN,
+			message: err.message,
+		});
+	});
 }
 
 function Find (call, cb) {
 	findBook(call.request.id)
 	.then(res => {
+		if (!res.id) {
+			return cb({
+				code: grpc.status.NOT_FOUND,
+				message: 'Not found',
+			});	
+		}
+
 		return cb(null, res)
-	})
+	}).catch(err => {
+		return cb({
+			code: grpc.status.UNKNOWN,
+			message: err.message,
+		});
+	});
 }
 
 function Add(call, cb) {
 	addBook(call.request)
 	.then(res => {
 		return cb(null, res)
-	})
+	}).catch(err => {
+		return cb({
+			code: grpc.status.UNKNOWN,
+			message: err.message,
+		});
+	});
 }
 
 function Remove(call, cb) {
 	removeBook(call.request.id)
 	.then(() => {
 		return cb(null, {})
-	})
+	}).catch(err => {
+		return cb({
+			code: grpc.status.UNKNOWN,
+			message: err.message,
+		});
+	});
 }
 
 // mongoClient.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`, (err, client) => {
@@ -120,7 +152,26 @@ mongoClient.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@mongodb:${DB_PORT}`, (e
 	const server = new grpc.Server()
 	server.addService(BookshopDefinition.BookshopService.service, {List, Find, Add, Remove})
 
-	server.bindAsync(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+	grpc.ServerCredentials.createSsl
+
+	// server.bindAsync(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
+	// 	if (err) {
+	// 		console.error('Error binding grpc: ' + err.stack);
+	// 		return;
+	// 	}
+	
+	// 	server.start()
+	// 	console.log(`Server listening to port ${port}...`)
+	// })
+
+	server.bindAsync(`${HOST}:${PORT}`, grpc.ServerCredentials.createSsl(
+		fs.readFileSync(caCertPath),
+		[{
+			cert_chain: fs.readFileSync(serverCertPath),
+			private_key: fs.readFileSync(serverKeyPath)
+		}],
+		false
+	), (err, port) => {
 		if (err) {
 			console.error('Error binding grpc: ' + err.stack);
 			return;
@@ -129,5 +180,4 @@ mongoClient.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@mongodb:${DB_PORT}`, (e
 		server.start()
 		console.log(`Server listening to port ${port}...`)
 	})
-
 });
